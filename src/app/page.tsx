6e9d0a7f9dff12c50480d8154d928c23958c315d
Iuri <ijiki16@freeuni.edu.ge>
2023-10-07 19:26:22 +0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import { getAllCharacters } from "../../lib/characters";
+import { VtmnButton } from "@vtmn/react";
 
 export default async function Home() {
   const data = await getAllCharacters();
@@ -10,6 +13,7 @@ export default async function Home() {
       {data?.map((item: any) => {
         return <div key={item.id}>{item.name}</div>;
       })}
+      <VtmnButton onClick={() => getAllCharacters()}>Refresh data</VtmnButton>
     </main>
   );
 }
