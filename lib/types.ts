00c573e14e85c4ae54dd27d8e2330b857e13da20
iuri <ijiki16@freeuni.edu.ge>
2023-10-08 11:42:47 +0400

    feat: add basic types

---
+++ lib/types.ts
@@ -0,0 +1,13 @@
+export interface Project {
+  url: string;
+  title: string;
+  text: string;
+}
+
+export interface User {
+  username: string;
+  password: string;
+  email: string;
+  name: string;
+  surname: string;
+}
