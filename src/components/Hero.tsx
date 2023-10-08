2fb4601801bbcf60876306d83c13f93115ea9258
amche13 <amchedlishvili94@gmail.com>
2023-10-08 08:52:45 +0400

    feat: hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,46 @@
+import { VtmnButton } from "@vtmn/react";
 import Image from "next/image";
 
 export const Hero = () => {
   return (
     <div style={{ position: "relative" }}>
-      <Image
-        src="/stars.png"
-        alt="hero"
-        width={500}
-        height={500}
-        style={{ width: "100%", height: "566px" }}
-      />
       <div>
-        <div>
-          <div>Marketplace for open science projects</div>
+        <div
+          style={{
+            backgroundImage: "url('/stars.png')",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100%",
+            height: "580px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              padding: "0 130px",
+              width: "500px",
+              boxSizing: "content-box",
+            }}
+          >
+            <div
+              style={{
+                marginBottom: "32px",
+                fontSize: "21px",
+                fontWeight: "bold",
+              }}
+            >
+              Marketplace for open science projects
+            </div>
+
+            <div style={{ fontSize: "18px", marginBottom: "40px" }}>
+              Portal that helps people who are looking for open-source projects
+              to work on and project creators who need skilled contributors to
+              find each other and communicate.
+            </div>
+
+            <VtmnButton>Find out more</VtmnButton>
+          </div>
         </div>
       </div>
     </div>
