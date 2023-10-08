c7bcbdd8a3654c07830f08f3358782ec09eaebcd
iuri <ijiki16@freeuni.edu.ge>
2023-10-08 12:50:55 +0400

    fix: remove dark mode

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,17 +4,24 @@ import { VtmnButton, VtmnSearch } from "@vtmn/react";
 export default function Projects() {
   return (
     <main>
-      <>
-        <VtmnButton>Refresh</VtmnButton>
-        <VtmnSearch
-          onClear={function noRefCheck() {}}
-          onSearch={function noRefCheck() {}}
-          placeholder="Search"
-          showSearchButton
-          size="medium"
-          variant="default"
-        />
-      </>
+      <div
+        style={{ display: "flex", flexDirection: "row", gap: 34, margin: 40 }}
+      >
+        <div>
+          <VtmnSearch
+            onClear={function noRefCheck() {}}
+            onSearch={function noRefCheck() {}}
+            placeholder="Search"
+            showSearchButton
+            size="medium"
+            variant="default"
+          />
+          <div>
+            <div>Filed of Since</div>
+          </div>
+        </div>
+        <div>Projects</div>
+      </div>
     </main>
   );
 }
