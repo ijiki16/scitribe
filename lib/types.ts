5b3ac9ff605288270ac81553d67e46a0d1e9cd78
iuri <ijiki16@freeuni.edu.ge>
2023-10-08 11:58:16 +0400

    feat: add project field of since

--- lib/types.ts
+++ lib/types.ts
@@ -2,6 +2,24 @@ export interface Project {
   url: string;
   title: string;
   text: string;
+  fieldOfSince: FieldOfSince;
+}
+
+export enum FieldOfSince {
+  Animals = "Animals",
+  ArchaeologyCultural = "Archaeology & cultural",
+  AstronomySpace = "Astronomy & space",
+  Biology = "Biology",
+  Birds = "Birds",
+  Chemistry = "Chemistry",
+  ClimateWeather = "Climate & weather",
+  ComputersTechnology = "Computers & Technology",
+  DisasterResponse = "Disaster response",
+  EcologyEnvironment = "Ecology & environment",
+  Education = "Education",
+  Food = "Food",
+  Geography = "Geography",
+  GeologyEarthScience = "Geology & earth science",
 }
 
 export interface User {
